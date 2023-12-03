f5062a1288162794b4bf448fb05baf6c65ec23db
Artem <144544684+Raha089@users.noreply.github.com>
2023-12-03 19:23:43 +0300

    Add files via upload

---
+++ 12 zanyatie/A5.py
@@ -0,0 +1,12 @@
+def vid(n):
+    if n < 10:
+        print(n)
+    else:
+        vid(n // 10)
+        print(n % 10)
+
+
+n = int(input("Введите натуральное число: "))
+
+print("Цифры числа N:")
+vid(n)
